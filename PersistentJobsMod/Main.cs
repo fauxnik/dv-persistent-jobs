301b82e3452c4626bd9a7cc1d2394dfc3d6cb2d9
Niko Fox <niko@fauxnik.me>
2020-06-16 03:53:39 -0700

    prevent player from starting job at destination
    * this is done by expiring the job if all associated train cars are too far away from the departure station
    * the chance of expiring a job under valid conditions is reduced by 1) allowing a single in-range train car to prevent expiration and 2) only checking at the time the job overview is inserted into the job validator

--- PersistentJobsMod/Main.cs
+++ PersistentJobsMod/Main.cs
@@ -1,44 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityModManagerNet;
 using Harmony12;
 using System.Reflection;
+using DV.Logic.Job;
 
 namespace PersistentJobsMod
 {
     static class Main
     {
+        private static UnityModManager.ModEntry thisModEntry;
+        private static bool isModBroken = false;
+        private static float initialDistanceRegular = 0f;
+        private static float initialDistanceAnyJobTaken = 0f;
+
         static void Load(UnityModManager.ModEntry modEntry)
         {
             var harmony = HarmonyInstance.Create(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            modEntry.OnToggle = OnToggle;
+            thisModEntry = modEntry;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool isTogglingOn)
+        {
+            if (isModBroken)
+            {
+                return !isTogglingOn;
+            }
+            thisModEntry.Active = isTogglingOn;
+            return true;
+        }
+
+        static void OnCriticalFailure()
+        {
+            isModBroken = true;
+            thisModEntry.Active = false;
         }
 
         [HarmonyPatch(typeof(StationController), "ExpireAllAvailableJobsInStation")]
         class StationController_ExpireAllAvailableJobsInStation_Patch
         {
-            [HarmonyPrefix]
-            static bool SkipJobExpiration()
+            static bool Prefix()
             {
-                return false; // skip the original method entirely
+                // skip the original method entirely when this mod is active
+                // doing so prevents jobs from expiring due to the player's distance from the station center
+                return !thisModEntry.Active;
             }
         }
 
-        [HarmonyPatch(typeof(StationJobGenerationRange), "Awake")]
-        class StationJobGenerationRange_Awake_Patch
+        [HarmonyPatch(typeof(StationJobGenerationRange))]
+        [HarmonyPatchAll]
+        class StationJobGenerationRange_AllMethods_Patch
         {
-            [HarmonyPrefix]
-            static void ExpandDestroyJobsSqrDistanceRegular(StationJobGenerationRange __instance)
+            static void Prefix(StationJobGenerationRange __instance, MethodBase __originalMethod)
             {
-                if (__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken < 4000000f)
+                try
                 {
-                    __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken = 4000000f;
+                    // backup existing values before overwriting
+                    if (initialDistanceRegular < 1f)
+                    {
+                        initialDistanceRegular = __instance.destroyGeneratedJobsSqrDistanceRegular;
+                    }
+                    if (initialDistanceAnyJobTaken < 1f)
+                    {
+                        initialDistanceAnyJobTaken = __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken;
+                    }
+
+                    if (thisModEntry.Active)
+                    {
+                        if (__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken < 4000000f)
+                        {
+                            __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken = 4000000f;
+                        }
+                        __instance.destroyGeneratedJobsSqrDistanceRegular = __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken;
+                    }
+                    else
+                    {
+                        __instance.destroyGeneratedJobsSqrDistanceRegular = initialDistanceRegular;
+                        __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken = initialDistanceAnyJobTaken;
+                    }
                 }
-                __instance.destroyGeneratedJobsSqrDistanceRegular = __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken;
+                catch (Exception e)
+                {
+                    Debug.LogError(string.Format("Exception thrown during StationJobGenerationRange.{0} prefix patch:\n{1}", __originalMethod.Name, e.Message));
+                    OnCriticalFailure();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(JobValidator), "ProcessJobOverview")]
+        class JobValidator_ProcessJobOverview_Patch
+        {
+            static void Prefix(List<StationController> ___allStations, DV.Printers.PrinterController ___bookletPrinter, JobOverview jobOverview)
+            {
+                try
+                {
+                    if (!thisModEntry.Active)
+                    {
+                        return;
+                    }
+
+                    Job job = jobOverview.job;
+                    StationController stationController = ___allStations.FirstOrDefault((StationController st) => st.logicStation.availableJobs.Contains(job));
+
+                    if (___bookletPrinter.IsOnCooldown || job.State != JobState.Available || stationController == null)
+                    {
+                        return;
+                    }
+
+                    // expire the job if all associated cars are outside the job destruction range at time of job overview processing
+                    // the base method's logic will handle generating the expired report
+                    StationJobGenerationRange stationRange = Traverse.Create(stationController).Field("stationRange").GetValue<StationJobGenerationRange>();
+                    Task taskWithCarsInRangeOfStation = job.tasks.FirstOrDefault((Task t) =>
+                    {
+                        List<Car> cars = Traverse.Create(t).Field("cars").GetValue<List<Car>>();
+                        Car carInRangeOfStation = cars.FirstOrDefault((Car c) =>
+                        {
+                            TrainCar trainCar = TrainCar.GetTrainCarByCarGuid(c.carGuid);
+                            return trainCar != null && (trainCar.transform.position - stationRange.stationCenterAnchor.position).sqrMagnitude <= stationRange.destroyGeneratedJobsSqrDistanceAnyJobTaken;
+                        });
+                        return carInRangeOfStation != null;
+                    });
+                    if (taskWithCarsInRangeOfStation == null)
+                    {
+                        job.ExpireJob();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(string.Format("Exception thrown during JobValidator.ProcessJobOverview prefix patch:\n{0}", e.Message));
+                    OnCriticalFailure();
+                }
             }
         }
     }
