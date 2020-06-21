2dbf159aaab0ad2ccd008b5eb4cb802f30148dde
Niko Fox <niko@fauxnik.me>
2020-06-21 00:13:37 -0700

    write train splitting logic for completed EmptyHaulJobs
    new JobChains will be generated by TransportJobChainProceduralGenerator (yet to be implemented)

--- PersistentJobsMod/Main.cs
+++ PersistentJobsMod/Main.cs
@@ -37,6 +37,11 @@ namespace PersistentJobsMod
         {
             isModBroken = true;
             thisModEntry.Active = false;
+            thisModEntry.Logger.Critical("Deactivating mod PersistentJobs due to critical failure!");
+            thisModEntry.Logger.Warning("You can reactivate PersistentJobs by restarting the game, but this failure " +
+                "type likely indicates an incompatibility between the mod and a recent game update. Please search the " +
+                "mod's Github issue tracker for a relevant report. If none is found, please open one. Include the" +
+                "exception message printed above and your game's current build number.");
         }
 
         [HarmonyPatch(typeof(StationController), "ExpireAllAvailableJobsInStation")]
@@ -84,7 +89,7 @@ namespace PersistentJobsMod
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError(string.Format("Exception thrown during StationJobGenerationRange.{0} prefix patch:\n{1}", __originalMethod.Name, e.Message));
+                    thisModEntry.Logger.Error(string.Format("Exception thrown during StationJobGenerationRange.{0} prefix patch:\n{1}", __originalMethod.Name, e.Message));
                     OnCriticalFailure();
                 }
             }
@@ -130,7 +135,116 @@ namespace PersistentJobsMod
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError(string.Format("Exception thrown during JobValidator.ProcessJobOverview prefix patch:\n{0}", e.Message));
+                    thisModEntry.Logger.Error(string.Format("Exception thrown during JobValidator.ProcessJobOverview prefix patch:\n{0}", e.Message));
+                    OnCriticalFailure();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(JobChainControllerWithEmptyHaulGeneration), "OnLastJobInChainCompleted")]
+        class JobChainControllerWithEmptyHaulGeneration_OnLastJobInChainCompleted_Patch
+        {
+            static void Prefix()
+            {
+                try
+                {
+                    // TODO: if possible, generate another TransportJobChain (instead of an EmptyHaulJob)
+                }
+                catch (Exception e)
+                {
+                    thisModEntry.Logger.Error(string.Format("Exception thrown during JobChainControllerWithEmptyHaulGeneration.OnLastJobInChainCompleted prefix patch:\n{0}", e.Message));
+                    OnCriticalFailure();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(JobChainController), "OnLastJobInChainCompleted")]
+        class JobChainController_OnLastJobInChainCompleted_Patch
+        {
+            static void Prefix(Job lastJobInChain, JobChainController __instance, List<StaticJobDefinition> ___jobChain)
+            {
+                try
+                {
+                    if (lastJobInChain.jobType != JobType.EmptyHaul)
+                    {
+                        thisModEntry.Logger.Warning(string.Format("Expected JobType.EmptyHaul but received {0}. Skipping job generation!", lastJobInChain.jobType.ToString()));
+                        return;
+                    }
+
+                    // generate one or more TransportJobChains
+                    // inspired by JobChainControllerWithEmptyHaulGeneration.OnLastJobInChainCompleted
+                    StaticEmptyHaulJobDefinition staticEmptyHaulJobDefinition = ___jobChain[___jobChain.Count - 1] as StaticEmptyHaulJobDefinition;
+                    if (staticEmptyHaulJobDefinition != null)
+                    {
+                        StationController startingStation = SingletonBehaviour<LogicController>.Instance.YardIdToStationController[staticEmptyHaulJobDefinition.chainData.chainDestinationYardId];
+                        System.Random rng = new System.Random(Environment.TickCount);
+                        Track track = staticEmptyHaulJobDefinition.destinationTrack;
+                        List<TrainCar> trainCars = Traverse.CreateWithType("JobChainControllerWithEmptyHaulGeneration")
+                            .Method("ExtractCorrespondingTrainCars")
+                            .GetValue<List<TrainCar>>(staticEmptyHaulJobDefinition.trainCarsToTransport);
+                        if (trainCars == null)
+                        {
+                            thisModEntry.Logger.Warning("Couldn't find all corresponding trainCars from trainCarsToTransport!");
+                            return;
+                        }
+                        // split the train into as few JobChains as possible
+                        List<JobChainControllerWithEmptyHaulGeneration> jobChainControllers = null;
+                        for (int lastCount = 0, div = 1; jobChainControllers == null && div <= trainCars.Count; div++)
+                        {
+                            int baseTrainCarsPerJob = trainCars.Count / div;
+                            List<int> trainCarsPerJob = new List<int>(div);
+                            for (int remainder = trainCars.Count % div - 1; remainder >= 0; remainder--)
+                            {
+                                trainCarsPerJob[remainder] += 1;
+                            }
+                            if (trainCarsPerJob[0] == lastCount)
+                            {
+                                // we've already tried a similar grouping and failed
+                                continue;
+                            }
+                            lastCount = trainCarsPerJob[0];
+                            List<JobChainControllerWithEmptyHaulGeneration> acc = new List<JobChainControllerWithEmptyHaulGeneration>();
+                            for (int jobIdx = 0; jobIdx < trainCarsPerJob.Count; jobIdx++)
+                            {
+                                List<TrainCar> subsetTrainCars = trainCars.GetRange(trainCarsPerJob.GetRange(0, jobIdx).Sum(), trainCarsPerJob[jobIdx]);
+                                JobChainControllerWithEmptyHaulGeneration jobChainController = TransportJobChainProceduralGenerator.GenerateTransportJobChainWithExistingCars(
+                                    startingStation,
+                                    track,
+                                    subsetTrainCars,
+                                    rng);
+                                if (jobChainController != null)
+                                {
+                                    acc.Add(jobChainController);
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            if (acc.Count == trainCarsPerJob.Count)
+                            {
+                                jobChainControllers = acc;
+                            }
+                        }
+                        if (jobChainControllers == null)
+                        {
+                            thisModEntry.Logger.Warning("Couldn't generate one or more JobChainController for trainCars from EmptyHaulJob!");
+                            return;
+                        }
+                        foreach (JobChainControllerWithEmptyHaulGeneration jobChainController in jobChainControllers)
+                        {
+                            foreach (TrainCar trainCar in jobChainController.trainCarsForJobChain)
+                            {
+                                __instance.trainCarsForJobChain.Remove(trainCar);
+                            }
+                            jobChainController.FinalizeSetupAndGenerateFirstJob();
+                            Debug.Log(string.Format("Generated job chain [{0}]: {1}", jobChainController.jobChainGO.name, jobChainController.jobChainGO));
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    thisModEntry.Logger.Error(string.Format("Exception thrown during JobChainController.OnLastJobInChainCompleted prefix patch:\n{0}", e.Message));
                     OnCriticalFailure();
                 }
             }
---
+++ PersistentJobsMod/TransportJobChainProceduralGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DV.Logic.Job;
+using UnityEngine;
+using System.Reflection;
+
+namespace PersistentJobsMod
+{
+    class TransportJobChainProceduralGenerator
+    {
+        public static JobChainControllerWithEmptyHaulGeneration GenerateTransportJobChainWithExistingCars(
+            StationController startingStation,
+            Track startingTrack,
+            List<TrainCar> existingTrainCars,
+            System.Random rng)
+        {
+            // TODO: implement this!
+        }
+    }
+}
