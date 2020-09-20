043a11778064912d0cc6ad5f0798ecd8d43efcc5
Niko Fox <niko@fauxnik.me>
2020-09-20 12:27:10 -0700

    patch PassengerJobs mod for compatibility

---
+++ PersistentJobsMod/Patches_PassengerJobsMod.cs
@@ -0,0 +1,38 @@
+﻿using Harmony12;
+using UnityEngine;
+
+namespace PersistentJobsMod
+{
+	class PJModSettings_PurgeData_Patch
+	{
+		static void Postfix()
+		{
+			Debug.Log("Clearing passenger spawning block list...");
+			Main.stationIdPassengerBlockList.Clear();
+		}
+	}
+
+	class PassengerJobGenerator_StartGenerationAsync_Patch
+	{
+		static bool Prefix(object __instance)
+		{
+			if (Main.modEntry.Active)
+			{
+				StationController controller = Traverse.Create(__instance).Field("Controller").GetValue<StationController>();
+				string stationId = controller?.logicStation?.ID;
+				return stationId == null || !Main.stationIdPassengerBlockList.Contains(controller.logicStation.ID);
+			}
+			return true;
+		}
+
+		static void Postfix(object __instance)
+		{
+			StationController controller = Traverse.Create(__instance).Field("Controller").GetValue<StationController>();
+			string stationId = controller?.logicStation?.ID;
+			if (stationId != null && !Main.stationIdPassengerBlockList.Contains(stationId))
+			{
+				Main.stationIdPassengerBlockList.Add(stationId);
+			}
+		}
+	}
+}
